11284240988bef37fe46028f01bfead5a212040c
Rohan Kumara <31560310+rohankumara@users.noreply.github.com>
2020-05-29 21:33:36 +0530

    updated

--- application.py
+++ application.py
@@ -10,9 +10,9 @@ import userFunctions
 def login():
     user=input("\n\nPlease enter your \n\t> MIT Employee ID exxx\n\tor \n\t> MIT Student ID sxxx:")
     if 'e' in user:
-        application.menuStaff()
+        menuStaff()
     elif 's' in user:
-        application.menuStudent()
+        menuStudent()
     else:
         print("Enter valid input again")
 
@@ -25,5 +25,40 @@ def menuStaff():
     print("E2. Add")
     print("E3. Display items")
     print("E4. Quit")
-    
-print(application.menuStaff())
+    option = input("Your option[E1-E4] : ")
+    if option == "E1":
+        userFunctions.buy_item('e')
+    elif option == "E2":
+        userFunctions.add_item()
+    elif option == "E3":
+        userFunctions.disp()
+    elif  option == "E4":
+        userFunctions.quit_vm()
+        
+
+
+
+
+def menuStudent():
+
+    print("*"*40)
+    print("     Employee Vending Machine    ")
+    print("*"*40)
+    print("S1. Buy")
+    print("S2. Display items")
+    print("S3. Quit")
+    option = input("Your option[S1-S3] : ")
+    if option == "S1":
+        userFunctions.buy_item('s')
+    elif option == "S2":
+        userFunctions.disp()
+    elif option == "S3":
+        userFunctions.quit_vm()
+       
+        
+
+
+while (True):
+    userFunctions.load_vm()
+    userFunctions.disp()
+    login()
